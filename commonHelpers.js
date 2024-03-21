1417ddccfe9966f27189a577f806e962c43cfe0d
danilkagugu <142350737+danilkagugu@users.noreply.github.com>
2024-03-21 23:29:06 +0000

    Deploying to gh-pages from @ danilkagugu/test12@d72600b754d4698796ccbe891b566bdecbf18f28 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,43 +1,43 @@
-import{S as l,N as m}from"./assets/vendor-094e1e92.js";(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const a of e)if(a.type==="childList")for(const s of a.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&i(s)}).observe(document,{childList:!0,subtree:!0});function r(e){const a={};return e.integrity&&(a.integrity=e.integrity),e.referrerPolicy&&(a.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?a.credentials="include":e.crossOrigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function i(e){if(e.ep)return;e.ep=!0;const a=r(e);fetch(e.href,a)}})();const d=[{url:"https://www.linkedin.com/in/danulo-yanishevskiy-27020a2b1/",small:"ilona-ratushniak1x.png",large:"ilona-ratushniak@2x.png",userNameEn:"Ilona Ratushniak",userNameUa:"Ілона Ратушняк",developer:"UI/UX Designer"},{url:"https://www.linkedin.com/in/danulo-yanishevskiy-27020a2b1/",small:"./img/team/maksym-nahornyi1x.png",large:"./img/team/maksym-nahornyi@2x.png",userNameEn:"Maksym Nahornyi",userNameUa:"Максим Нагорний",developer:"PM"},{url:"https://www.linkedin.com/in/danulo-yanishevskiy-27020a2b1/",small:"./img/team/danulo-yanishevskiy1x.png",large:"./img/team/danulo-yanishevskiy@2x.png",userNameEn:"Yanishevskiy Danulo",userNameUa:"Янішевський Данило",developer:"frontend"},{url:"http://linkedin.com/in/vladyslav-hnatok-671a092b4",small:"./img/team/vladyslav-marchenko1x.png",large:"./img/team/vladyslav-hnatok@2x.png",userNameEn:"Vladyslav Hnatok",userNameUa:"Владислав Гнаток",developer:"frontend"},{url:"www.linkedin.com/in/sofiia-koval-19151b296",small:"./img/team/sofiia-koval1x.png",large:"./img/team/sofiia-koval@2x.png",userNameEn:"Sofiia Koval",userNameUa:"Софія Коваль",developer:"frontend"},{url:"https://www.linkedin.com/in/sergiy-myronenko-5668b9292/",small:"./img/team/sergiy-myronenko1x.png",large:"./img/team/sergiy-myronenko@2x.png",userNameEn:"Sergiy Myronenko",userNameUa:"Сергій Мироненко",developer:"frontend"},{url:"https://www.linkedin.com/in/oleksandr-horobiy/",small:"./img/team/oleksandr-horobiy1x.png",large:"./img/team/oleksandr-horobiy@2x.png",userNameEn:"Oleksandr Horobiy",userNameUa:"Олександр Горобій",developer:"frontend"},{url:"https://www.linkedin.com/in/oleksandra-shevchuk-8270b62a0/",small:"./img/team/oleksandra-shevchuk1x.png",large:"./img/team/oleksandra-shevchuk@2x.png",userNameEn:"Oleksandra Shevchuk",userNameUa:"Олександра Шевчук",developer:"frontend"},{url:"https://www.linkedin.com/in/mykyta-poddubnyi-3897032bb/",small:"./img/team/mykyta-poddubnyi1x.png",large:"./img/team/mykyta-poddubnyi@2x.png",userNameEn:"Mykyta Poddubnyi",userNameUa:"Микита Поддубний",developer:"frontend"},{url:"https://www.linkedin.com/in/olenaishchenko/",small:"./img/team/olena-ishchenko1x.png",large:"./img/team/olena-ishchenko@2x.png",userNameEn:"Olena Ishchenko",userNameUa:"Олена Іщенко",developer:"frontend"},{url:"https://www.linkedin.com/in/-svitlana-klymenko/",small:"./img/team/svitlana-klymenko1x.png",large:"./img/team/svitlana-klymenko@2x.png",userNameEn:"Svitlana Klymenko",userNameUa:"Світлана Клименко",developer:"frontend"},{url:"https://www.linkedin.com/in/alla-sopinska-92a5532b1/",small:"./img/team/alla-sopinska1x.png",large:"./img/team/alla-sopinska@2x.png",userNameEn:"Alla Sopinska",userNameUa:"Алла Сопінська",developer:"frontend"},{url:"https://www.linkedin.com/in/danulo-yanishevskiy-27020a2b1/",small:"./img/team/nataliia-boicheniuk1x.png",large:"./img/team/nataliia-boicheniuk@2x.png",userNameEn:"Nataliia Boicheniuk",userNameUa:"Наталія Бойченюк",developer:"QA"},{url:"https://www.linkedin.com/in/danulo-yanishevskiy-27020a2b1/",small:"./img/team/svetlana-skvortsova1x.png",large:"./img/team/svetlana-skvortsova@2x.png",userNameEn:"Svetlana Skvortsova",userNameUa:"Світлана Скворцова",developer:"QA"},{url:"https://www.linkedin.com/in/danulo-yanishevskiy-27020a2b1/",small:"./img/team/alyona-maslianyk1x.png",large:"./img/team/alyona-maslianyk@2x.png",userNameEn:"Alyona Maslianyk",userNameUa:"Альона Маслянюк",developer:"QA"},{url:"https://www.linkedin.com/in/oleh-khimchenko-4a25aa296/  ",small:"./img/team/oleh-khimchenko1x.png",large:"./img/team/oleh-khimchenko@2x.png",userNameEn:"Oleh Khimchenko",userNameUa:"Олег Кімченко",developer:"QA"},{url:"https://www.linkedin.com/in/danulo-yanishevskiy-27020a2b1/",small:"./img/team/vladyslav-marchenko1x.png",large:"./img/team/vladyslav-marchenko@2x.png",userNameEn:"Vladyslav Marchenko",userNameUa:"Владислав Марченко",developer:"QA"},{url:"https://www.linkedin.com/in/danulo-yanishevskiy-27020a2b1/",small:"./img/team/viktoria-bairak1x.png",large:"./img/team/viktoria-bairak@2x.png",userNameEn:"Viktoria Bairak",userNameUa:"Вікторія Байрак",developer:"QA"}],p="/test12/assets/symbol-7352cb7a.svg",c={modules:[m],breakpoints:{375:{slidesPerView:2,spaceBetween:35},768:{slidesPerView:3,spaceBetween:50},1e3:{slidesPerView:4,spaceBetween:50},1440:{slidesPerView:6,spaceBetween:50}},navigation:{nextEl:".swiper-button-next-section-team",prevEl:".swiper-button-prev-section-team"},slidesOffsetAfter:25,slidesOffsetBefore:10};new l(".swiper",c);const u=document.querySelector(".swiper-wrapper"),g=document.querySelector(".developer-section"),k=document.querySelector(".icon-close-section-team"),h=document.querySelector(".team-button");function t(){const o=g.classList.toggle("is-open-section-team");document.body.style.overflow=o?"hidden":""}k.addEventListener("click",t);h.addEventListener("click",t);const v=()=>d.map(({small:n,large:r,userNameEn:i,developer:e,url:a,userNameUa:s})=>`<div class="swiper-slide">
+import{S as c,N as l}from"./assets/vendor-094e1e92.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const n of s.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function r(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function i(e){if(e.ep)return;e.ep=!0;const s=r(e);fetch(e.href,s)}})();const d=[{url:"https://www.linkedin.com/in/danulo-yanishevskiy-27020a2b1/",small:"ilona-ratushniak1x.png",large:"ilona-ratushniak@2x.png",userNameEn:"Ilona Ratushniak",userNameUa:"Ілона Ратушняк",developer:"UI/UX Designer"},{url:"https://www.linkedin.com/in/danulo-yanishevskiy-27020a2b1/",small:"maksym-nahornyi1x.png",large:"maksym-nahornyi@2x.png",userNameEn:"Maksym Nahornyi",userNameUa:"Максим Нагорний",developer:"PM"},{url:"https://www.linkedin.com/in/danulo-yanishevskiy-27020a2b1/",small:"danulo-yanishevskiy1x.png",large:"danulo-yanishevskiy@2x.png",userNameEn:"Yanishevskiy Danulo",userNameUa:"Янішевський Данило",developer:"frontend"}],u="/test12/assets/symbol-7352cb7a.svg",p={modules:[l],breakpoints:{375:{slidesPerView:2,spaceBetween:35},768:{slidesPerView:3,spaceBetween:50},1e3:{slidesPerView:4,spaceBetween:50},1440:{slidesPerView:6,spaceBetween:50}},navigation:{nextEl:".swiper-button-next-section-team",prevEl:".swiper-button-prev-section-team"},slidesOffsetAfter:25,slidesOffsetBefore:10};new c(".swiper",p);const m=document.querySelector(".swiper-wrapper"),f=document.querySelector(".developer-section"),g=document.querySelector(".icon-close-section-team"),v=document.querySelector(".team-button");function a(){const o=f.classList.toggle("is-open-section-team");document.body.style.overflow=o?"hidden":""}g.addEventListener("click",a);v.addEventListener("click",a);const y=()=>d.map(({small:t,large:r,userNameEn:i,developer:e,url:s,userNameUa:n})=>`<div class="swiper-slide">
       <div class="developer-container">
   <div class="container-img">
     <div class="box-img-team">
       <div class="icon-linkedin-team">
-      <a href="${a}"  target="_blank"
+      <a href="${s}"  target="_blank"
         >
         <svg class="linkedin" width="16" height="16">
-          <use href=${p}#icon-linkedin></use>
+          <use href=${u}#icon-linkedin></use>
         </svg>
         </a>
       </div>
-      <a href="${a}" target="_blank"
+      <a href="${s}" target="_blank"
         >
 <picture>
   <source
     media="(min-width: 768px )"
     srcset="
-      ./img/team/${n}    1x,
-      ./img/team/${r} 2x
+      img/team/${t}    1x,
+      img/team/${r} 2x
     "
   />
   <source
     media="(max-width: 767.98px)"
     srcset="
-      ./img/team/${n}    1x,
-      ./img/team/${r} 2x
+      img/team/${t}    1x,
+      img/team/${r} 2x
     "
   />
   <img
     class="dev-photo"
-    src="./img/team/${n}"
+    src="img/team/${t}"
     alt="${i}"
   />
 </picture>
         </a>
     </div>
   </div>
-  <h3 class="dev-name" data-ua="${s}" data-en="${i}">${i}</h3>
+  <h3 class="dev-name" data-ua="${n}" data-en="${i}">${i}</h3>
   <p class="dev-desription" >${e}</p>
   </div>
-</div>`).join("");u.innerHTML=v();
+</div>`).join("");m.innerHTML=y();
 //# sourceMappingURL=commonHelpers.js.map
